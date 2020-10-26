fc23f9fdddf8dfbe883f854fcb27711ff81ec355
ExtraTerrestrial2187 <ethan.hall.2017@gmail.com>
2020-10-26 11:44:43 +1000

    djent

---
+++ o.py
@@ -0,0 +1,9 @@
+from sense_hat import SenseHat
+sense=SenseHat()
+sense.clear()
+while True:
+    o=sense.get_orientation()
+    pitch=o["pitch"]
+    roll=o["roll"]
+    yaw=o["yaw"]
+    print(f"Pitch:{pitch}\tRoll:{roll}\tYaw:{yaw}")

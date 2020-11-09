7251e7134710e5cedd96a14dd59e614f2cc53d61
ExtraTerrestrial2187 <ethan.hall.2017@gmail.com>
2020-11-09 11:46:11 +1000

    HOW CLEAR MESSAGE PARTYWAY THROUGH HOW???

--- ISS.py
+++ ISS.py
@@ -3,13 +3,23 @@ sense=SenseHat()
 sense.clear()
 
 while True:
-    temp=sense.get_temperature()
-    temp=round(temp,2)
-    print(temp)
-    sense.show_message(str(temp))
-    
-# stick code
-#sense.stick.direction_up=red
-#sense.stick.direction_down=blue
-#sense.stick.direction_right=yellow
-#sense.stick.direction_left=green
+    for event in sense.stick.get_events():
+        if event.action=='pressed':
+            sense.clear()
+            if event.direction=='up':
+                temp=sense.get_temperature()
+                temp=round(temp,2)
+                print(temp)
+                sense.show_message(str(temp))
+                #inputs stack up and output all at once after
+                #idk how to clear message partway through
+            elif event.direction=='left':
+                pres=sense.get_pressure()
+                pres=round(pres,2)
+                print(pres)
+                sense.show_message(str(pres))
+            elif event.direction=='right':
+                humid=sense.get_humidity()
+                humid=round(humid,2)
+                print(humid)
+                sense.show_message(str(humid))
